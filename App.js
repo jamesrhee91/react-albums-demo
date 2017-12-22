625771d3f4fded5215653d08917fd9934e175923
jamesrhee91 <jamesshrhee@gmail.com>
2017-12-21 22:07:14 -0500

    creating albums demo

--- App.js
+++ App.js
@@ -1,66 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+// Import a library to help create a Component
+import React from 'react';
+import { Text } from 'react-native';
 
-import React, { Component } from 'react';
-import { Router, Scene } from 'react-native-router-flux'
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-import Authentication from './routes/Authentication'
-import HomePage from './routes/HomePage'
+// Create a Component
+const App = () => {
+  return (
+    <Text>Some Text</Text>
+  );
+};
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component<{}> {
-  render() {
-    return (
-      <Router>
-        <Scene key='root'>
-          <Scene
-            component={Authentication}
-            hideNavBar={true}
-            initial={true}
-            key='Authentication'
-            title='Authentication'
-          />
-          <Scene
-            component={HomePage}
-            hideNavBar={true}
-            key='HomePage'
-            title='Home Page'
-          />
-        </Scene>
-      </Router>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default App;
