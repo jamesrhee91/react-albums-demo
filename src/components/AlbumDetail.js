e127d87fb577c3dc1dea3c3db8667934bc63411c
jamesrhee91 <jamesshrhee@gmail.com>
2017-12-22 18:42:46 -0500

    added button component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,26 +1,52 @@
 import React from 'react'
-import { Text, View, Image } from 'react-native'
+import { Text, View, Image, Linking } from 'react-native'
 import Card from './Card'
 import CardSection from './CardSection'
+import Button from './Button'
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image } = album
-  const { thumbnailStyle, headerContentStyle } = styles
+  const { title, artist, thumbnail_image, image, url } = album
+  const {
+    thumbnailStyle,
+    headerContentStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle
+  } = styles
+
+  const handlePress = () => {
+    Linking.openURL(url)
+  }
 
   return (
     <Card>
+
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image
             style={thumbnailStyle}
             source={{ uri: thumbnail_image }}
           />
         </View>
         <View style={headerContentStyle}>
-          <Text>{title}</Text>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
       </CardSection>
+
+      <CardSection>
+        <Image
+          style={imageStyle}
+          source={{ uri: image }}
+        />
+      </CardSection>
+
+      <CardSection>
+        <Button onPress={handlePress}>
+          Buy Now
+        </Button>
+      </CardSection>
+
     </Card>
   )
 }
@@ -29,6 +55,9 @@ const styles = {
   headerContentStyle: {
     flexDirection: 'column',
     justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
   },
   thumbnailStyle: {
     height: 50,
@@ -39,6 +68,11 @@ const styles = {
     alignItems: 'center',
     marginLeft: 10,
     marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 }
 
