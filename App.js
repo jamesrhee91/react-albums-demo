3e8bd67ee4dd304327a0139c2af1a2689f412eaa
jamesrhee91 <jamesshrhee@gmail.com>
2017-12-21 22:51:14 -0500

    styling to header

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 // Import a library to help create a Component
-import React from 'react';
-import { Text } from 'react-native';
+import React from 'react'
+import Header from './src/components/header'
 
 // Create a Component
 const App = () => {
   return (
-    <Text>Some Text</Text>
-  );
-};
+    <Header />
+  )
+}
 
 export default App;
